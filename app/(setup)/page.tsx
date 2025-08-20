cda865baacb037709880278dc0dfc04e72b42665
Yousuf-Wizdan <mdyousufwizdan@gmail.com>
2025-08-21 01:39:08 +0530

    Initial Modal UI Setup

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,27 @@
+import InitialModal from '@/components/modals/InitialModal';
+import { initalProfile } from '@/lib/initial-profile'
+import { db } from '@/lib/prisma';
+import { redirect } from 'next/navigation';
+import React from 'react'
+
+const SetUpPage = async () => {
+    const profile = await initalProfile();
+    // some is a Prisma filter operator that means: “At least one related record should satisfy this condition.”
+    // basically here to find which server is user joined in 
+    const server = await db.server.findFirst({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    })
+    if(server){
+        return redirect(`/servers/${server.id}`)
+    }
+
+  return <InitialModal />
+}
+
+export default SetUpPage
