3ae4c8e3eb94fc7c9ffa66b0b005bf39b1d9a7c5
Yousuf-Wizdan <mdyousufwizdan@gmail.com>
2025-08-21 22:15:06 +0530

    eslit msg

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+   // Ignore all files in src/generated/
+  {
+    ignores: ["lib/generated/**/*.{js,ts}"],
+  },
 ];
 
 export default eslintConfig;
