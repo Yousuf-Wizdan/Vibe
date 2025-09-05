67faeebb78f3da865cf9141916cd1f798ff1dd68
Yousuf-Wizdan <mdyousufwizdan@gmail.com>
2025-09-05 20:05:17 +0530

    Added Channel ID Page

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,9 +1,52 @@
+import { currentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/prisma';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const ServerPage = async () => {
+interface ServerPageProps {
+  params: Promise<{ serverId: string }>
+}
 
+const ServerPage = async ({ params }: ServerPageProps) => {
+
+  const serverId = (await params).serverId;
+
+  const profile = await currentProfile()
+  if (!profile) {
+    return redirect('/sign-in')
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: serverId,
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
+    },
+    include: {
+      channels: {
+        where: {
+          name: 'General'
+        },
+        orderBy: {
+          createdAt: 'asc'
+        }
+      }
+    }
+  })
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== 'General') {
+    return null;
+  }
+
+
+  // what this page does basically always redirect to general channel whenever user click the server
   return (
-    <div>ServerPage</div>
+    redirect(`/servers/${serverId}/channels/${initialChannel?.id}`)
   )
 }
 
--- components/server/ServerChannel.tsx
+++ components/server/ServerChannel.tsx
@@ -6,7 +6,7 @@ import { Edit, Hash, Lock, Mic, Trash, Video } from 'lucide-react'
 import { useParams, useRouter } from 'next/navigation'
 import React from 'react'
 import ActionToolTip from '../ActionToolTip'
-import { useModel } from '@/hooks/use-model-store'
+import { ModalType, useModel } from '@/hooks/use-model-store'
 
 interface ServerChannelProps {
     channel: Channel,
@@ -32,9 +32,19 @@ const ServerChannel = ({
 
     const Icon = iconMap[channel.type]
 
+    const onClick = () => {
+        router.push(`/servers/${server.id}/channels/${channel.id}`)
+    }
+
+    const onAction = (e: React.MouseEvent , action: ModalType) => {
+        // stops event bubbling does not effect parent div only effects child
+        e.stopPropagation();
+        onOpen(action , {server , channel})
+    }
+
   return (
     <button
-        onClick={() => {}}
+        onClick={onClick}
         className={cn(
             "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
             params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
@@ -51,7 +61,7 @@ const ServerChannel = ({
             <div className='ml-auto flex items-center gap-x-2'>
                 <ActionToolTip label='Edit'>
                     <Edit 
-                        onClick={() => onOpen('editChannel' , {server , channel})}
+                        onClick={(e) => onAction(e ,'editChannel')}
                         className='hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition'
                     />
                 </ActionToolTip>
@@ -59,7 +69,7 @@ const ServerChannel = ({
                     label='Delete'
                 >
                     <Trash 
-                        onClick={() => onOpen('deleteChannel' , { channel , server })}
+                        onClick={(e) => onAction(e ,'deleteChannel')}
                         className='hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition'
                     />
                 </ActionToolTip>
